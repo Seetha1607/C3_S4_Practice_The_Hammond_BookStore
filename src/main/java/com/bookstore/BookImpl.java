d9a9f238904ed155631a3c079a7b270cb15ec53d
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 14:34:50 +0530

    created book object and initialized the values

--- src/main/java/com/bookstore/BookImpl.java
+++ src/main/java/com/bookstore/BookImpl.java
@@ -10,6 +10,7 @@ package com.bookstore;
 public class BookImpl {
     public static void main(String[] args) {
         Author author = new Author("Ann Cleeves", "Cleeves");
+        Book book = new Book("The Rising Tide", 2456789, "It's a Crime Novel", 55.0, author);
 
     }
 }
