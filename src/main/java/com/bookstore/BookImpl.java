a8a2e745899553bc53e45cf0e7db73c99bf620c8
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 14:54:33 +0530

    changes done

--- src/main/java/com/bookstore/BookImpl.java
+++ src/main/java/com/bookstore/BookImpl.java
@@ -9,10 +9,20 @@ package com.bookstore;
 
 public class BookImpl {
     public static void main(String[] args) {
+
         // Creating an object of the Author class.
-        Author author = new Author("Ann Cleeves", "Cleeves");
+        Author author = new Author();
+        author.setAuthorName("Ann cleeves");
+        author.setAuthorPenName("Cleeves");
+
         // Creating an object of the Book class.
-        Book book = new Book("The Rising Tide", 2456789, "It's a Crime Novel", 55.0, author);
+        Book book = new Book();
+        book.setTitle("The Rising Tide");
+        book.setIsbnNumber(34678356);
+        book.setDescription("It's a Crime Novel");
+        book.setPrice(55.0);
+        book.setAuthor(author);
+
         // Calling the method displayBookDetails() from the Book class.
         book.displayBookDetails();
     }
