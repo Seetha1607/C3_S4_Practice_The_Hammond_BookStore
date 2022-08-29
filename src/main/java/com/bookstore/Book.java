819227dfc29bb4233285827f4b98266f25a4c441
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 12:24:42 +0530

    added the getter and setter methods for the fields in book class

--- src/main/java/com/bookstore/Book.java
+++ src/main/java/com/bookstore/Book.java
@@ -22,4 +22,36 @@ public class Book {
         this.description = description;
         this.price = price;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setIsbnNumber(int isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
