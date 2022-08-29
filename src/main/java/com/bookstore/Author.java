02904efbf377a7c57d0e04ce58f259393cc1ef9e
Seetha <seethalakshmi1607@gmail.com>
2022-08-29 12:49:41 +0530

    added getter and setter method in author class

--- src/main/java/com/bookstore/Author.java
+++ src/main/java/com/bookstore/Author.java
@@ -19,6 +19,22 @@ public class Author {
         this.authorPenName = authorPenName;
     }
 
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorPenName() {
+        return authorPenName;
+    }
+
+    public void setAuthorPenName(String authorPenName) {
+        this.authorPenName = authorPenName;
+    }
+
     @Override
     public String toString() {
         return "Author{" +
